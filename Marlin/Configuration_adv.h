be8b0a918e9673f89409106ca6641e7a0a979e05
screech24 <72464022+screech24@users.noreply.github.com>
2020-11-29 00:02:43 -0500

    KP3S-Stock-NO-ABL

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1203,7 +1203,7 @@
 
   //#define BROWSE_MEDIA_ON_INSERT          // Open the file browser when media is inserted
 
-  #define EVENT_GCODE_SD_ABORT "G1X0Y170Z10F1000"      // G-code to run on SD Abort Print (e.g., "G28XY" or "G27")
+  #define EVENT_GCODE_SD_ABORT "G27 P2"      // G-code to run on SD Abort Print (e.g., "G28XY" or "G27")
 
   #if ENABLED(PRINTER_EVENT_LEDS)
     #define PE_LEDS_COMPLETED_TIME  (30*60) // (seconds) Time to keep the LED "done" color before restoring normal illumination
